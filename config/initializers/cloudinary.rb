983f68a93cedf186fa89e1cc7c120c24f9a2a3b4
raulescu <raul.leul@yahoo.com>
2016-08-12 22:39:49 +0200

    Add Cloudinary

---
+++ config/initializers/cloudinary.rb
@@ -0,0 +1,6 @@
+Cloudinary.config do |config|
+  config.cloud_name = 'dg1jctv6y'
+  config.api_key = '688611778145883'
+  config.api_secret = 'L0GkVqgLe__COpz95dhFuLPekr8'
+  config.cdn_subdomain = true
+end
